e6a472d0c8d391f5efd134a27819c6ed51d0d2df
jhangkar <taqit3852@gmail.com>
2023-05-09 02:18:21 +0600

    complete

---
+++ scripts/app.js
@@ -0,0 +1,173 @@
+
+
+
+const infos = [
+    "As anyone may observe from the outside, the most influential people in the world today are not Buddhists. On top of that anyone from a Non-Buddhist city, will tell you that they have not heard from a Buddhist about why that path is best. ",
+    "It is a sickness is the Buddha communities today to ignore the first vow, and try to move on to 2,3, and 4. By the very means of the vows, step 2 cannot be done without first completing step 1. Step 1 is in fact, an earthly desire. The Four Main Vows are a harmonious right of passage that must be balanced in action towards their completion.",
+    "Therefore, the way of the Buddha should be to renounce all worldly desires IN ORDER TO save all people, and to learn all teachings IN ORDER TO save all people, and that once these steps are completed perfect enlightenment will be attained. This means that these tasks are in harmony. As one renounces more wordy desires to spend more time saving people, one learns teachings to improve how they go about saving others, and rides the collective path towards enlightenment. ",
+    "However, instead of following this path and order, Buddhists today are known as people continually meditating. Struggling to awaken and renounce all worldly desires, and instead all acting to each other like they are farther along the path. I believe Buddhism has become a sick game of ineffective spiritual ego stroking.",
+    "I am not saying that the Men and Women who have become great at meditating for hours and wholly living a minimalist life have not gone further down their path of enlightenment, instead that it is very sad that so much potential is moving towards the wrong place for the world to awaken and be saved. It is a fact these people who realize their potential to follow the first vow will be some of the strongest Buddhas there are.",
+    "The funny thing about tech entrepreneurs who are rapidly changing the future, and Buddhism is that these tech entrepreneurs are slowly learning the way of the Buddha in order to optimize themselves. They are reaching new paths of enlightenment. Instead of referring to their actions as the Buddha way, they are learning the teachings of Buddha in order to be more effective. ",
+
+]
+const exapmles = [
+    'A few examples of parallel movements to the 10 precepts are:',
+    'Not to engage in licentious acts or encourage others to do so. A monk is expected to abstain from sexual conduct entirely.',
+    'Not to use false words and speech, or encourage others to do so.',
+    'Not to trade or sell alcoholic beverages or encourage others to do so.',
+    'Not to harbor anger or encourage others to be angry.',
+]
+
+const listExamples = [
+    "A disciple of the Buddha shall not himself kill, encourage others to kill, kill by expedient means, praise killing, rejoice at witnessing killing, or kill through incantation or deviant mantras. He must not create the causes, conditions, methods, or karma of killing, and shall not intentionally kill any living creature. As a Buddha's disciple, he ought to nurture a mind of compassion and filial piety, always devising expedient means to rescue and protect all beings. If instead, he fails to restrain himself and kills sentient beings without mercy, he commits a Parajika (major) offense.",
+    "A disciple of the Buddha must not himself steal or encourage others to steal, steal by expedient means, steal by means of incantation or deviant mantras. He should not create the causes, conditions, methods, or karma of stealing. No valuables or possessions, even those belonging to ghosts and spirits or thieves and robbers, be they as small as a needle or blade of grass, may be stolen. As a Buddha's disciple, he ought to have a mind of mercy, compassion, and filial piety -- always helping people earn merits and achieve happiness. If instead, he steals the possessions of others, he commits a Parajika offense.",
+    "A disciple of the Buddha must not engage in licentious acts or encourage others to do so. [As a monk] he should not have sexual relations with any female -- be she a human, animal, deity or spirit -- nor create the causes, conditions, methods, or karma of such misconduct. Indeed, he must not engage in improper sexual conduct with anyone. A Buddha's disciple ought to have a mind of filial piety -- rescuing all sentient beings and instructing them in the Dharma of purity and chastity. If instead, he lacks compassion and encourages others to engage in sexual relations promiscuously, including with animals and even their mothers, daughters, sisters, or other close relatives, he commits a Parajika offense.",
+    "A disciple of the Buddha must not himself use false words and speech, or encourage others to lie or lie by expedient means. He should not involve himself in the causes, conditions, methods, or karma of lying, saying that he has seen what he has not seen or vice versa, or lying implicitly through physical or mental means. As a Buddha's disciple, he ought to maintain Right Speech and Right Views always, and lead all others to maintain them as well. If instead, he causes wrong speech, wrong views or evil karma in others, he commits a Parajika offense.",
+    "A disciple of the Buddha must not trade in alcoholic beverages or encourage others to do so. He should not create the causes, conditions, methods, or karma of selling any intoxicant whatsoever, for intoxicants are the causes and conditions of all kinds of offenses. As a Buddha's disciple, he ought to help all sentient beings achieve clear wisdom. If instead, he causes them to have upside-down, topsy-turvy thinking, he commits a Parajika offense.",
+    "A disciple of the Buddha must not himself broadcast the misdeeds or infractions of Bodhisattva-clerics or Bodhisattva-laypersons, or of [ordinary] monks and nuns -- nor encourage others to do so. He must not create the causes, conditions, methods, or karma of discussing the offenses of the assembly. As a Buddha's disciple, whenever he hears evil persons, externalists or followers of the Two Vehicles speak of practices contrary to the Dharma or contrary to the precepts within the Buddhist community, he should instruct them with a compassionate mind and lead them to develop wholesome faith in the Mahayana. If instead, he discusses the faults and misdeeds that occur within the assembly, he commits a Parajika offense.",
+    "A disciple of the Buddha shall not praise himself and speak ill of others, or encourage others to do so. He must not create the causes, conditions, methods, or karma of praising himself and disparaging others. As a disciple of the Buddha, he should be willing to stand in for all sentient beings and endure humiliation and slander -- accepting blame and letting sentient beings have all the glory. If instead, he displays his own virtues and conceals the good points of others, thus causing them to suffer slander, he commits a Parajika offense.",
+    "A disciple of the Buddha must not be stingy or encourage others to be stingy. He should not create the causes, conditions, methods, or karma of stinginess. As a Bodhisattva, whenever a destitute person comes for help, he should give that person what he needs. If instead, out of anger and resentment, he denies all assistance -- refusing to help with even a penny, a needle, a blade of grass, even a single sentence or verse or a phrase of Dharma, but instead scolds and abuses that person -- he commits a Parajika offense.",
+    "A disciple of the Buddha shall not harbor anger or encourage others to be angry. He should not create the causes, conditions, methods, or karma of anger. As a disciple of the Buddha, he ought to be compassionate and filial, helping all sentient beings develop the good roots of non-contention. If instead, he insults and abuses sentient beings, or even transformation beings [such as deities and spirits], with harsh words, hitting them with his fists or feet, or attacking them with a knife or club -- or harbors grudges even when the victim confesses his mistakes and humbly seeks forgiveness in a soft, conciliatory voice -- the disciple commits a Parajika offense.",
+    "A Buddha's disciple shall not himself speak ill of the Triple Jewel or encourage others to do so. He must not create the causes, conditions, methods or karma of slander. If a disciple hears but a single word of slander against the Buddha from externalists or evil beings, he experiences a pain similar to that of three hundred spears piercing his heart. How then could he possibly slander the Triple Jewel himself? Hence, if a disciple lacks faith and filial piety towards the Triple Jewel, and even assists evil persons or those of aberrant views to slander the Triple Jewel, he commits a Parajika offense."
+
+]
+const messageList = [
+    'To save all people: this is by far the biggest vow that is failed to be recognized by modern Buddhist society. Outreach is a non-factor. So many people who need this valuable information are left in the dark, with no outreach from Buddhists. Actual progress using technology (tools to increase impact) are completely disregarded.',
+    'Many people have regarded this initial vow as meaning to renounce. But here’s what the true text says “Desires for tangible things (such as wealth, property, or other material goods) or for pleasures of the body (such as sexual activity, gluttony, or other hedonistic pursuits). Buddhism teaches us to try to let go of our worldly desires, freeing our minds and bodies for a state of enlightenment.”As it says, freeing our minds and bodies for a state of enlightenment.',
+    'Learning all teachings requires knowing all things, we do not even know how consciousness works yet. To learn all teachings, we need to learn by the act of doing. There is no better thing to learn at this time, than how to spread change like Buddha did.',
+    '“In <strong>Buddhism, enlightenment</strong> (called bodhi in Indian <strong>Buddhism</strong>, or satori in Zen <strong></strong>Buddhism) is when a Buddhist finds the truth about life and stops being reborn because they have reached Nirvana.” In order to stop the cycle of rebirth, we must stop the death to life cycle. With infinite time our particles are always brought back to a conscious form. Until we find a conscious form to reach Nirvana in, we will repeat the cycle. “<strong>Rebirth</strong> in <a href="https://en.m.wikipedia.org/wiki/Buddhism" class="message-link" target="_blank">Buddhism</a> refers to its teaching that the actions of a person lead to a new existence after death, in an endless cycle called <a href="https://en.m.wikipedia.org/wiki/Sa%E1%B9%83s%C4%81ra" class="message-link" target="_blank">saṃsāra</a>.[1][2] This cycle is considered to be <a href="https://en.m.wikipedia.org/wiki/Du%E1%B8%A5kha" class="message-link" target="_blank">dukkha</a>, unsatisfactory and painful. The cycle stops only if <a href="https://en.m.wikipedia.org/wiki/Moksha" class="message-link" target="_blank">liberation</a> is achieved by <a href="https://en.m.wikipedia.org/wiki/Enlightenment_in_Buddhism" class="message-link" target="_blank">insight</a> and the <a href="https://en.m.wikipedia.org/wiki/Nirvana" class="message-link" target="_blank">extinguishing of craving</a>.'
+]
+
+const sources = [
+    {
+        title: 'Four Truths, Four Vows',
+        description: 'This is another in my series of discussions of ideas Stephen Batchelor has been presenting in dharma talks since late 2010. You can hear them at dharmaseed.org.One of the attractive ideas to come out of Stephen Batchelor’s recent teaching is a mapping of the Four Noble Truths onto the Four Bodhisattva Vows of the Zen tradition, a concept Batchelor got from former Zen priest and current Vipassana teacher Gil Fronsdal.',
+        link: 'https://secularbuddhism.org/four-truths-four-vows/',
+        image: ''
+    },
+    {
+        title: 'List of Buddha claimants',
+        description: 'This is a list of notable people who have claimed to have attained enlightenment and become buddhas, claimed to be manifestations of bodhisattvas, identified themselves as Gautama Buddha or Maitreya Buddha, or been honored as buddhas or bodhisattvas.',
+        link: 'https://en.wikipedia.org/wiki/List_of_Buddha_claimants',
+        image: ''
+    },
+    {
+        title: 'Homepage',
+        description: 'Every year since 1983, Daisaku Ikeda, as the SGI president, has issued a peace proposal, exploring the interrelation between core Buddhist concepts and the',
+        link: 'https://www.worldtribune.org/peace-proposal/',
+        image: 'https://www.worldtribune.org/wp-content/uploads/sites/2/2022/03/Intro_DM_WT_040-scaled.jpg'
+    },
+]
+
+
+
+const writeDocument = (id, arr, element, className) => {
+    const infoContainer = document.getElementById(id);
+    arr.forEach(info => {
+        const p = document.createElement(element);
+        p.innerHTML = info
+        p.classList.add(className)
+        infoContainer.appendChild(p);
+    })
+}
+writeDocument('info-container', infos, 'p', 'info-paragraph')
+writeDocument('example-container', exapmles, 'p', 'example-paragraph')
+writeDocument('message-list-container', messageList, 'li', 'message-list-item')
+
+
+// toggle icon button and load examples
+const toggleButton = document.getElementById('toggleButton');
+let activeDropdown = false;
+const listExampleContainer = document.getElementById('list-container-example')
+toggleButton.addEventListener('click', () => {
+    listExampleContainer.innerHTML = ""
+    listExampleContainer.classList.toggle('example-animate')
+    if (!activeDropdown) {
+        // writeDocument('list-container-example',listExamples,'li','list-item')
+        const infoContainer = document.getElementById('list-container-example');
+        listExamples.forEach(element => {
+            const li = document.createElement('li');
+            let newElement = "";
+            let i = 0;
+            const intervalId = setInterval(() => {
+                if (i < element.length) {
+                    newElement += element[i];
+                    li.innerHTML = newElement;
+                    i++;
+                } else {
+                    clearInterval(intervalId);
+                }
+            }, 1);
+            li.classList.add('list-item')
+            infoContainer.appendChild(li);
+        })
+
+
+    }
+    toggleButton.classList.toggle('active')
+    activeDropdown = !activeDropdown;
+})
+
+const sourcesContainer = document.getElementById("source-card-container")
+sources.forEach(source => {
+    const a = document.createElement("a")
+    a.innerHTML = `<div class='source-card-box'>
+    <div>
+        <h3 class="source-title">${source?.title}</h3>
+        <p>${source?.description && source?.description}</p>
+        <small>${source.link}</small>
+    </div>
+    <div>
+        ${source?.image && `<img src=${source?.image} alt="">`
+        }
+    </div>
+</div>`
+    a.setAttribute('target', '_blank')
+    a.setAttribute('href', source.link)
+    a.classList.add('source-card')
+    sourcesContainer.appendChild(a)
+})
+
+// search text in paragraphs
+
+
+const searchBox = document.getElementById('search-box');
+const searchBtn = document.getElementById('search-btn');
+
+searchBtn.addEventListener('click', () => {
+
+    const searchText = searchBox.value.toLowerCase();
+
+    const paragraphs = document.getElementsByTagName('p');
+
+    let firstMatch = true;
+    
+    for (let i = 0; i < paragraphs.length; i++) {
+        const paragraph = paragraphs[i];
+        const paragraphText = paragraph.textContent.toLowerCase();
+
+        if (paragraphText.includes(searchText)) {
+ 
+            if (firstMatch) {
+                paragraph.scrollIntoView({ behavior: 'smooth' });
+                firstMatch = false;
+            }
+
+            const highlightedText = paragraphText.replace(new RegExp(searchText, 'gi'), `<span class="highlight">${searchText}</span>`);
+            paragraph.innerHTML = highlightedText;          
+        }
+    }
+});
+
+
+// dark and light mode
+
+const toggleMode = document.getElementById('mode-toggle')
+const sunIcon = document.getElementById('toggle-sun')
+const moonIcon = document.getElementById('toggle-moon')
+
+toggleMode.addEventListener('click', () =>{
+    sunIcon.classList.toggle('hide')
+    moonIcon.classList.toggle('hide')
+    document.body.classList.toggle('dark-mode')
+})
